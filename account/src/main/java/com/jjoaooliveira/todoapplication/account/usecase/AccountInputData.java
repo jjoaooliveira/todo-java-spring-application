8eafbac1cef92380c5c6538a6f709ca3bf793ed5
jotinha <jjoaooliveira555@gmail.com>
2025-05-07 12:55:46 -0400

    feat(account): create AccountInputData

---
+++ account/src/main/java/com/jjoaooliveira/todoapplication/account/usecase/AccountInputData.java
@@ -0,0 +1,44 @@
+package com.jjoaooliveira.todoapplication.account.usecase;
+
+import java.util.UUID;
+
+public class AccountInputData {
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+
+    public AccountInputData(UUID id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public AccountInputData(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
