21d4abfda2ed0f4b561ea135c86eec28afcde3fb
jotinha <jjoaooliveira555@gmail.com>
2025-05-13 19:47:54 -0400

    feat(account): change IReadAccountDataGateway interface to IAccountDataGateway

--- account/src/main/java/com/jjoaooliveira/todoapplication/account/usecase/IAccountDataGateway.java
+++ account/src/main/java/com/jjoaooliveira/todoapplication/account/usecase/IAccountDataGateway.java
@@ -1,7 +1,10 @@
 package com.jjoaooliveira.todoapplication.account.usecase;
 
+import java.util.Optional;
+
 import com.jjoaooliveira.todoapplication.account.entity.Account;
 
 public interface IAccountDataGateway {
     Account saveAccount(Account account);
+    Optional<Account> getAccountByEmail(String email);
 }
--- account/src/main/java/com/jjoaooliveira/todoapplication/account/usecase/user/ReadAccountImpl.java
+++ account/src/main/java/com/jjoaooliveira/todoapplication/account/usecase/user/ReadAccountImpl.java
@@ -1,19 +1,23 @@
 package com.jjoaooliveira.todoapplication.account.usecase.user;
 
+import com.jjoaooliveira.todoapplication.account.usecase.exception.AccountNotFoundException;
+
 import com.jjoaooliveira.todoapplication.account.entity.Account;
 import com.jjoaooliveira.todoapplication.account.usecase.AccountOutputData;
+import com.jjoaooliveira.todoapplication.account.usecase.IAccountDataGateway;
 
 public class ReadAccountImpl implements IReadAccount {
 
-    private final IReadAccountDataGateway dataGateway;
+    private final IAccountDataGateway dataGateway;
 
-    public ReadAccountImpl(IReadAccountDataGateway dataGateway) {
+    public ReadAccountImpl(IAccountDataGateway dataGateway) {
         this.dataGateway = dataGateway;
     }
 
     @Override
     public AccountOutputData readAccount(String email) {
-        Account returnedAccount = dataGateway.read(email);
+        Account returnedAccount = dataGateway.getAccountByEmail(email)
+            .orElseThrow(() -> new AccountNotFoundException("Account not found"));
         
         return new AccountOutputData(
             returnedAccount.getEmail(), 
