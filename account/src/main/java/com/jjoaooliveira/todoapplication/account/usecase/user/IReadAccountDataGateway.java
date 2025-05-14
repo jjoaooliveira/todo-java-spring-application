41d71b6d2171e84d25f6df689a958b49b2e63093
jotinha <jjoaooliveira555@gmail.com>
2025-05-13 21:50:02 -0400

    feat(account): remove IReadAccountDataGateway

--- account/src/main/java/com/jjoaooliveira/todoapplication/account/usecase/user/IReadAccountDataGateway.java
+++
@@ -1,7 +0,0 @@
-package com.jjoaooliveira.todoapplication.account.usecase.user;
-
-import com.jjoaooliveira.todoapplication.account.entity.Account;
-
-public interface IReadAccountDataGateway {
-    Account read(String email);
-}
