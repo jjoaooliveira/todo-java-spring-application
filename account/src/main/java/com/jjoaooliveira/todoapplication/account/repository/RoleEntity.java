f0c7d8258f9c00ed55e4e00c51148b6721bd4497
jotinha <jjoaooliveira555@gmail.com>
2025-05-15 16:59:27 -0400

    feat(account): create RoleEntity

---
+++ account/src/main/java/com/jjoaooliveira/todoapplication/account/repository/RoleEntity.java
@@ -0,0 +1,38 @@
+package com.jjoaooliveira.todoapplication.account.repository;
+
+import java.util.Set;
+import java.util.UUID;
+
+import com.jjoaooliveira.todoapplication.account.entity.Role;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "roles")
+public class RoleEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<AccountEntity> account;
+
+    public RoleEntity() {}
+    
+    public RoleEntity(UUID id, Role role) {
+        this.id = id;
+        this.role = role;
+    }
+}
