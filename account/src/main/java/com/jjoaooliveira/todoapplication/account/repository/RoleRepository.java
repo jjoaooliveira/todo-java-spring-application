87f09ddc438598f2b09e47fec2dda4b7fac264b2
jotinha <jjoaooliveira555@gmail.com>
2025-05-19 15:29:10 -0400

    Refactor(account): change findByRole parameter type

--- account/src/main/java/com/jjoaooliveira/todoapplication/account/repository/RoleRepository.java
+++ account/src/main/java/com/jjoaooliveira/todoapplication/account/repository/RoleRepository.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.jjoaooliveira.todoapplication.account.entity.Role;
+
 public interface RoleRepository extends JpaRepository<RoleEntity, UUID> {
-    Optional<RoleEntity> findByRole(String role);
+    Optional<RoleEntity> findByRole(Role role);
 }
